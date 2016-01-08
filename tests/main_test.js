88c523f58c11047012a174e382ace31354764c7f
Torsten Link <torsten.link@db.com>
2016-01-08 16:52:30 +0100

    WIP

--- tests/main_test.js
+++ tests/main_test.js
@@ -13,43 +13,55 @@ const path = require("path");
 const fixturesDir = path.join(__dirname, 'fixtures');
 
 
-const manager = require('kronos-service-manager').manager;
+const ksm = require('kronos-service-manager');
+
+ksm.manager().then(manager => {
+	console.log('started');
+}, err => {
+	console.log("err");
+	console.log(err);
+}).catch(function (err) {
+	console.log("err");
+	console.log(err);
+});
+
 const flow = require('kronos-flow');
 const inboundFile = require('kronos-adapter-inbound-file');
 const outboundFile = require('kronos-adapter-outbound-file');
 
 
-manager().then(function (manager) {
-	console.log('started');
-
-
-	// ---------------------------
-	// register all the steps
-	// ---------------------------
-	flow.registerWithManager(manager);
-	inboundFile.registerWithManager(manager);
-	outboundFile.registerWithManager(manager);
-
-	// ---------------------------
-	// load the flows
-	// ---------------------------
-	const flowDefintion = require(path.join(fixturesDir, 'main-flow.json'));
-	flow.flow.loadFlows(manager, manager.scopeReporter, flowDefintion);
-
-	// get the flow from the manager
-	const myFlow = manager.getFlow('untar-applications');
-
-	myFlow.start().then(function (step) {
-		console.log('flow: started');
-
-
-
-	}.catch(function (err) {
-		console.log(err);
-	}));
-
-
-
-}).catch(function (err) {
-	console.log(err);
-});
+// manager().then(function (manager) {
+// 		console.log('started');
+//
+//
+// 		// ---------------------------
+// 		// register all the steps
+// 		// ---------------------------
+// 		flow.registerWithManager(manager);
+// 		inboundFile.registerWithManager(manager);
+// 		outboundFile.registerWithManager(manager);
+//
+// 		// ---------------------------
+// 		// load the flows
+// 		// ---------------------------
+// 		const flowDefintion = require(path.join(fixturesDir, 'main-flow.json'));
+// 		flow.flow.loadFlows(manager, manager.scopeReporter, flowDefintion);
+//
+// 		// get the flow from the manager
+// 		const myFlow = manager.getFlow('untar-applications');
+//
+// 		myFlow.start().then(function (step) {
+// 			console.log('flow: started');
+//
+//
+//
+// 		}.catch(function (err) {
+// 			console.log(err);
+// 		}));
+//
+//
+//
+// 	},
+// 	function (err) {
+// 		console.log(err)
+// 	});
