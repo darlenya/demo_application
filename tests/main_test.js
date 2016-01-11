bb9d4d8cf2d36ba40442e1f334852dd8f618a257
Torsten Link <torsten.link@db.com>
2016-01-11 22:01:18 +0100

    WIP

--- tests/main_test.js
+++ tests/main_test.js
@@ -11,58 +11,59 @@ const fs = require('fs');
 const path = require("path");
 
 const fixturesDir = path.join(__dirname, 'fixtures');
+const flow = require('kronos-flow');
+const inboundFile = require('kronos-adapter-inbound-file');
+const outboundFile = require('kronos-adapter-outbound-file');
+const archiveTar = require('kronos-step-archive-tar');
 
 
 const ksm = require('kronos-service-manager');
 
-// ***** Warum geht das nicht?????
-ksm.manager().then(manager => {
-	console.log('started');
-}, err => {
-	console.log("err");
-	console.log(err);
-}).catch(function (err) { // ***** Braucht man Catch wenn man in 'then' eine zweite funktion hat?
-	console.log("err");
-	console.log(err);
+
+describe('main', function () {
+	it('test', function (done) {
+
+
+		ksm.manager().then(function (manager) {
+				console.log('started');
+
+
+				// ---------------------------
+				// register all the steps
+				// ---------------------------
+				inboundFile.registerWithManager(manager);
+				outboundFile.registerWithManager(manager);
+				archiveTar.registerWithManager(manager);
+				flow.registerWithManager(manager);
+
+				// ---------------------------
+				// load the flows
+				// ---------------------------
+
+				const filePath = path.join(fixturesDir, 'main-flow.json');
+				const fileContent = fs.readFileSync(filePath, 'utf8');
+				const flowDefintion = JSON.parse(fileContent);
+
+				flow.loadFlows(manager, manager.scopeReporter, flowDefintion);
+
+				// get the flow from the manager
+				const myFlow = manager.flows['untar-applications'];
+
+				console.log('start the flow');
+
+				myFlow.start().then(function (step) {
+					console.log('flow: started');
+					done();
+				}).catch(function (err) {
+					console.log(err);
+				});
+
+
+
+			},
+			function (err) {
+				console.log(err);
+			});
+
+	});
 });
-
-const flow = require('kronos-flow');
-const inboundFile = require('kronos-adapter-inbound-file');
-const outboundFile = require('kronos-adapter-outbound-file');
-
-
-// manager().then(function (manager) {
-// 		console.log('started');
-//
-//
-// 		// ---------------------------
-// 		// register all the steps
-// 		// ---------------------------
-// 		flow.registerWithManager(manager);
-// 		inboundFile.registerWithManager(manager);
-// 		outboundFile.registerWithManager(manager);
-//
-// 		// ---------------------------
-// 		// load the flows
-// 		// ---------------------------
-// 		const flowDefintion = require(path.join(fixturesDir, 'main-flow.json'));
-// 		flow.flow.loadFlows(manager, manager.scopeReporter, flowDefintion);
-//
-// 		// get the flow from the manager
-// 		const myFlow = manager.getFlow('untar-applications');
-//
-// 		myFlow.start().then(function (step) {
-// 			console.log('flow: started');
-//
-//
-//
-// 		}.catch(function (err) {
-// 			console.log(err);
-// 		}));
-//
-//
-//
-// 	},
-// 	function (err) {
-// 		console.log(err)
-// 	});
